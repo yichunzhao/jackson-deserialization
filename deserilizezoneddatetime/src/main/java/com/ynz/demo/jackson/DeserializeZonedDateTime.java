fd2684e0d198c88d675073010e973541d9ccefb4
yichunzhao <yichun.zhao@autoremind.com>
2022-12-30 10:37:54 +0100

    moving user json string to resources as a File

--- deserilizezoneddatetime/src/main/java/com/ynz/demo/jackson/DeserializeZonedDateTime.java
+++ deserilizezoneddatetime/src/main/java/com/ynz/demo/jackson/DeserializeZonedDateTime.java
@@ -1,23 +1,21 @@
 package com.ynz.demo.jackson;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.ynz.demo.jackson.model.User;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class DeserializeZonedDateTime {
 
-    private static final String jsonString = "{\n" +
-            "  \"name\": \"test_1f5374568a05\",\n" +
-            "  \"createdAt\": \"2017-03-22T06:45:56.284+01:00\"\n" +
-            "}";
-
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = DeserializeZonedDateTime.class.getResourceAsStream("/user.json");
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
 
-        User user = objectMapper.readValue(jsonString, User.class);
+        User user = objectMapper.readValue(inputStream, User.class);
         System.out.println(user);
     }
 }
